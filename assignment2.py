37f861a46e858f6a93b74cd8f5b64639538e54ad
husker00a <58126169+husker00a@users.noreply.github.com>
2025-02-28 15:19:40 -0600

    RandomForest2

--- assignment2.py
+++ assignment2.py
@@ -1,6 +1,5 @@
 import pandas as pd
-import numpy as np
-from xgboost import XGBClassifier
+from sklearn.ensemble import RandomForestClassifier as RF
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
@@ -9,6 +8,14 @@ trainData = pd.read_csv("https://github.com/dustywhite7/Econ8310/raw/master/Assi
 y = trainData['meal']
 x = trainData.drop(['meal','id','DateTime'], axis=1)
 
-model = XGBClassifier(n_estimators=50, max_depth=3,learning_rate=0.5, objective='multi:softmax')
-# Fit to our training split
+# Create the model and fit it
+model = RF(n_estimators=100, n_jobs=-1, max_depth=5)
 modelFit = model.fit(x, y)
+
+# Load test data, then separate x and y variables
+testData = pd.read_csv("https://github.com/dustywhite7/Econ8310/raw/master/AssignmentData/assignment3test.csv")
+y = testData['meal']
+x = testData.drop(['meal','id','DateTime'], axis=1)
+
+# Test our model using the testing data
+pred = model.predict(x)
